d5f1c04a84bbf44cc1a96087e206097159caff91
Farhan <farhan.farasdak@bukalapak.com>
2020-11-18 21:21:08 +0700

    use faraday

--- lib/user/client.rb
+++ lib/user/client.rb
@@ -17,33 +17,48 @@ module User
       @hostname = hostname
     end
 
+    def initialize(params = {})
+      # Default is set to telolet kube staging
+      @host = params[:host]
+      @user = params[:username]
+      @pass = params[:password]
+      @source = params[:source]
+      @hostname = params[:hostname]
+
+      @http_client = Faraday.new(url: @host) do |conn|
+        conn.request    :retry, max: 3, interval: 0.05,
+                                interval_randomness: 0.5, backoff_factor: 2
+        conn.adapter    Faraday.default_adapter
+        conn.basic_auth @user, @pass
+
+      end
+    end
+
+
     PATH = {
       get_user_by_id: '/_internal/users/id/',
     }
 
     def get_user_by_id(user_id, options = {})
       url = @url + PATH[:get_user_by_id] + user_id.to_s
-      auth = {
-        username: @username,
-        password: @password
-      }
+      get_data(url)
+    end
 
-      params = {
-        headers: {
-          content_type: :json,
-          accept: :json
-          host: @hostname,
-          "BL-Service": @source
-        },
-        basic_auth: auth
-      }
+
+    private
+
+    def get_data(path)
       begin
-        HTTParty.get(url, params)
+        @http_client.get do |req|
+          req.url path
+          req.headers['Content-Type'] = 'application/json'
+          req.headers['BL-Service'] = @source
+          req.headers['Host'] = @hostname
+        end
       rescue StandardError => e
         # TODO: log here
         raise e
       end
     end
-
   end
 end
